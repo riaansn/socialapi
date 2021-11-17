9d8bdaa9bda7a6c3634026ca2f47dc420c53881f
riaan <riaan@clev-ads.co.za>
2021-11-17 12:00:36 +0200

    Added phone number field

--- api/models.py
+++ api/models.py
@@ -28,6 +28,7 @@ class User(Base):
     password = Column(String, nullable=False)
     create_at = Column(TIMESTAMP(timezone=True),
                        nullable=False, server_default=text('now()'))
+    phone_number = Column(String)
 
 
 class Vote(Base):
