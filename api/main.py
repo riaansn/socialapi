3b42cee454e5f837d4e1c30b11d3b793f4352142
riaan <riaan@clev-ads.co.za>
2021-11-17 14:04:48 +0200

    upload api

--- api/main.py
+++ api/main.py
@@ -8,7 +8,7 @@ from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
 
 # models.Base.metadata.create_all(bind=engine) # only needed if not using alembic
-origins = []
+origins = ["*"]
 
 app.add_middleware(
     CORSMiddleware,
